450288bd7cadf6f614b00423cc054f1f5633e7f2
Puria Nafisi Azizi <puria@dyne.org>
2019-08-30 16:55:45 +0200

    💰 Adding scoring functions

--- svmr.js
+++ svmr.js
@@ -17,22 +17,21 @@ const config = {
 };
 
 const settings = {
-  player: {
-    size: 100,
-    velocity: 2000
-  }
+  velocity: 2000,
+  score: 0,
+  scoreMultiplier: 1.0
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("player", "assets/images/puria.png"); //, { frameWidth: settings.player.size, frameHeight: settings.player.size })
+  this.load.image("player", "assets/images/puria.png");
   this.load.image("villain", "assets/images/baffo.png");
 }
 
 function create() {
   cursors = this.input.keyboard.createCursorKeys();
-  player = this.physics.add.image(100, 450, "player");
+  player = this.physics.add.image(0, config.height, "player");
   player.setCollideWorldBounds(true);
   platforms = this.physics.add.staticGroup();
   const villains = this.physics.add.group();
@@ -41,11 +40,27 @@ function create() {
 
 function update() {
   if (cursors.left.isDown) {
-    player.setVelocityX(-settings.player.velocity);
+    player.setVelocityX(-settings.velocity);
   } else if (cursors.right.isDown) {
-    player.setVelocityX(settings.player.velocity);
+    player.setVelocityX(settings.velocity);
   } else if (player.x === config.width - player.body.halfWidth ||
              player.x === player.body.halfWidth) {
     player.setVelocityX(0);
   }
+}
+
+function computeScore() {
+    settings.score += settings.score * settings.scoreMultiplier
+}
+
+function speedUpScoring(multiplier) {
+    settings.scoreMultiplier *= multiplier
+}
+
+function slowDownScoring(multiplier) {
+    settings.scoreMultiplier /= multiplier
+}
+
+function getScore() {
+    return settings.score
 }
