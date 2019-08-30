d882f8fc81fb7097ce83ce062c549c0f23f10cc5
Puria Nafisi Azizi <puria@dyne.org>
2019-08-30 20:49:09 +0200

    🍱 Adding new sprite

--- svmr.js
+++ svmr.js
@@ -6,7 +6,7 @@ const config = {
     default: "arcade",
     arcade: {
       gravity: { y: 200 },
-      debug: true
+      debug: false
     }
   },
   scene: {
@@ -20,26 +20,33 @@ const settings = {
   velocity: 2000,
   score: 1,
   scoreMultiplier: 1.0,
-  scoreMessage: "",
+  scoreMessage: ""
 };
 
 const game = new Phaser.Game(config);
 let timedEvent;
 
 function preload() {
-  this.load.image("player", "assets/images/puria.png");
+  this.load.image("player", "assets/images/ape.svg");
   this.load.image("villain", "assets/images/baffo.png");
 }
 
 function create() {
   cursors = this.input.keyboard.createCursorKeys();
-  player = this.physics.add.image(0, config.height, "player");
+  player = this.physics.add.image(0, config.height, "player").setScale(0.6);
   player.setCollideWorldBounds(true);
   platforms = this.physics.add.staticGroup();
   const villains = this.physics.add.group();
   this.physics.add.collider(player, villains);
-  settings.scoreMessage = this.add.text(20, 20, "PUNTI: " + settings.score, {fill: "#FFF"});
-  timedEvent = this.time.addEvent({ delay: 1000, callback: computeScore, callbackScope: this, loop: true });
+  settings.scoreMessage = this.add.text(20, 20, "PUNTI: " + settings.score, {
+    fill: "#FFF"
+  });
+  timedEvent = this.time.addEvent({
+    delay: 1000,
+    callback: computeScore,
+    callbackScope: this,
+    loop: true
+  });
 }
 
 function update() {
@@ -47,21 +54,23 @@ function update() {
     player.setVelocityX(-settings.velocity);
   } else if (cursors.right.isDown) {
     player.setVelocityX(settings.velocity);
-  } else if (player.x === config.width - player.body.halfWidth ||
-             player.x === player.body.halfWidth) {
+  } else if (
+    player.x === config.width - player.body.halfWidth ||
+    player.x === player.body.halfWidth
+  ) {
     player.setVelocityX(0);
   }
 }
 
 function computeScore() {
-    settings.score += settings.scoreMultiplier
-    settings.scoreMessage.setText("PUNTI: " + settings.score);
+  settings.score += settings.scoreMultiplier;
+  settings.scoreMessage.setText("PUNTI: " + settings.score);
 }
 
 function speedUpScoring(multiplier) {
-    settings.scoreMultiplier *= multiplier
+  settings.scoreMultiplier *= multiplier;
 }
 
 function slowDownScoring(multiplier) {
-    settings.scoreMultiplier /= multiplier
+  settings.scoreMultiplier /= multiplier;
 }
