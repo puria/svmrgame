6a693cedc53fc28e779ecc1a313bda960b59ced4
Alessandro Miracapillo <alesim11@gmail.com>
2019-08-31 16:20:22 +0200

    Update svmr.js
    first abstraction of multiple floors :cactus:

--- svmr.js
+++ svmr.js
@@ -25,6 +25,9 @@ const settings = {
 
 const game = new Phaser.Game(config);
 let timedEvent;
+var player = [];
+
+var numeroPiste = 3;
 
 function preload() {
   this.load.image("player", "assets/images/ape.svg");
@@ -33,23 +36,27 @@ function preload() {
 
 function create() {
   key = this.input.keyboard.createCursorKeys();
-  setupPlayers(this);
-  setupScoring(this);
+  for(i = 0; i < numeroPiste; i++){
+  setupPlayers(this, i);
+  setupInvisibles(this, i);
+  setupScoring(this, i);
+  }
 }
 
 function update() {
-  handleKeys();
+  for(i = 0; i < numeroPiste; i++){
+  handleKeys(this,i);
+}
 }
 
-function handleKeys() {
-  if (key.left.isDown) {
-    moveLeft(player);
-  } else if (key.right.isDown) {
-    moveRight(player);
-  } else if (isLeftBound(player) || isRightBound(player)) {
-    player.setVelocityX(0);
-  }
+function handleKeys(game, i) {
+ if (key.left.isDown) {
+ game.physics.arcade.moveToXY(player[i],500,0,0,3000);
+ }
 }
+
+
+  
 
 function isLeftBound(player) {
   return player.x === config.width - player.body.halfWidth;
@@ -80,22 +87,27 @@ function slowDownScoring(multiplier) {
   settings.scoreMultiplier /= multiplier;
 }
 
-function setupPlayers(game) {
-  player = game.physics.add.image(0, config.height, "player").setScale(0.6);
-  player.setCollideWorldBounds(true);
-  var rightLine = new Phaser.Geom.Line(config.width, 0, config.width, config.height);
-  var leftLine = new Phaser.Geom.Line(0, 0, 0, config.height);
-  var rightVillains = game.add.group({ key: 'villain', frameQuantity: 4 });
-  var leftVillains = game.add.group({ key: 'villain', frameQuantity: 4 });
-  Phaser.Actions.RandomLine(rightVillains.getChildren(), rightLine);
-  Phaser.Actions.RandomLine(leftVillains.getChildren(), leftLine);
+function setupPlayers(game,i) {
+  var sezioni = config.width/numeroPiste;
+  var rightLine = [];
+  var leftLine = [];
+  var rightVillains = [];
+  var leftVillains = [];
+  player[i] = game.physics.add.image(config.width/numeroPiste*i, config.height, "player").setScale(0.6);
+  player[i].setCollideWorldBounds(true);
+  leftLine[i] = new Phaser.Geom.Line(config.width/numeroPiste*i, 0, config.width/numeroPiste*i, config.height);
+  rightLine[i] = new Phaser.Geom.Line(config.width/numeroPiste*(i+1), 0, config.width/numeroPiste*(i+1), config.height);
+  rightVillains[i] = game.add.group({ key: 'villain', frameQuantity: 1 });
+  leftVillains[i] = game.add.group({ key: 'villain', frameQuantity: 1 });
+  Phaser.Actions.RandomLine(rightVillains[i].getChildren(), rightLine[i]);
+  Phaser.Actions.RandomLine(leftVillains[i].getChildren(), leftLine[i]);  
 }
 
 function setupScoring(game) {
   settings.scoreMessage = game.add.text(20, 20, "PUNTI: " + settings.score, {
     fill: "#FFF"
-  });
-  timedEvent = game.time.addEvent({
+   });
+   timedEvent = game.time.addEvent({
     delay: 1000,
     callback: computeScore,
     callbackScope: game,
